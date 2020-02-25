ebca4959edc0a861d65be2381ef8172e958dc793
Michael Kuczynski <mkuczynski@outlook.com>
2020-02-25 12:49:57 -0600

    repeated steps for missing field for all fields
    To make sure user enters values for all fields

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,29 @@
 function inputValues()
 {
 	var user = document.forms["signup_form"]["userName"].value;
+	var pass = document.forms["signup_form"]["password"].value;
+	var name = document.forms["signup_form"]["fullName"].value;
+	var emailAd = document.forms["signup_form"]["email"].value;
+	var phoneNum = document.forms["signup_form"]["phone"].value;
+
 	if(user=="")
 	{
 		alert("Please Enter a username");
+	}
+	else if(pass=="")
+	{
+		alert("Please Enter a password");
+	}
+	else if(name=="")
+	{
+		alert("Please Enter a valid name");
+	}
+	else if(emailAd=="")
+	{
+		alert("Please Enter an email address");
+	}
+	else if(phoneNum=="")
+	{
+		alert("Please Enter a Phone number");
 	}
 }
